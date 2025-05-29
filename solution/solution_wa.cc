5b914caf32283b1ec353c91b868d0f17d99c6af9
pps-admin <pps.github@gmail.com>
2025-05-29 21:24:54 +0900

    Update: path 'solution/solution_wa.cc' - Updated by 'notice99'

--- solution/solution_wa.cc
+++ solution/solution_wa.cc
@@ -1,8 +1,8 @@
-#include <cstdio>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a + b + 1);
+    int a;
+    cin >> a;
+    cout << a*2/2;
     return 0;
 }
