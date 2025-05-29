ced284d70a345ffd8445162ae863223dac936f4d
pps-admin <pps.github@gmail.com>
2025-05-29 21:42:16 +0900

    Update: path 'checker/tcmp.cc' - Updated by 'notice99'

--- checker/tcmp.cc
+++ checker/tcmp.cc
@@ -1,19 +1,24 @@
 #include "testlib.h"
+#include <string>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char* argv[]) {
     registerTestlibCmd(argc, argv);
 
-    long long expected = ans.readInt();
-    long long actual = ouf.readInt();
+    string expected_str = ans.readToken();
+    string actual_str = ouf.readToken();
+
+    long long expected = stoll(expected_str);
+    long long actual = stoll(actual_str);
 
     if (!ouf.seekEof()) {
         quitf(_wa, "Participant output has extra content.");
     }
 
     if (expected != actual) {
-        quitf(_wa, "Wrong answer: expected %d, found %d", expected, actual);
+        quitf(_wa, "Wrong answer: expected %lld, found %lld", expected, actual);
     }
 
-    quitf(_ok, "Correct answer: %d", expected);
+    quitf(_ok, "Correct answer: %lld", expected);
 }
