ab1688764b063e56342cf226e763089cf1b5e90b
pps-admin <pps.github@gmail.com>
2025-05-29 21:25:34 +0900

    Update: path 'generator/generator.cc' - Updated by 'notice99'

--- generator/generator.cc
+++ generator/generator.cc
@@ -9,12 +9,9 @@ int main(int argc, char** argv) {
 
     al = atoi(argv[1]);
     ar = atoi(argv[2]);
-    bl = atoi(argv[3]);
-    br = atoi(argv[4]);
 
     a = rnd.next(al, ar);
-    b = rnd.next(bl, br);
 
-    printf("%d %d\n", a, b);
+    printf("%d %d\n", a);
     return 0;
 }
